2881367197667f87eaf7651a78d54bb118bf162f
Bill Franklin <b.franklin@outlook.com>
2023-12-16 17:10:40 +0000

    enable uninstalling k3s from agents

--- manifests/uninstall.pp
+++ manifests/uninstall.pp
@@ -1,6 +1,12 @@
 # @summary Class responsible for uninstalling k3s
 class k3s::uninstall {
-  exec { '/usr/local/bin/k3s-uninstall.sh':
-    onlyif => '/usr/bin/test -f /usr/local/bin/k3s-uninstall.sh',
+  if $k3s::operation_mode == 'server' {
+    exec { '/usr/local/bin/k3s-uninstall.sh':
+      onlyif => '/usr/bin/test -f /usr/local/bin/k3s-uninstall.sh',
+    }
+  } else {
+    exec { '/usr/local/bin/k3s-agent-uninstall.sh':
+      onlyif => '/usr/bin/test -f /usr/local/bin/k3s-agent-uninstall.sh',
+    }
   }
 }
